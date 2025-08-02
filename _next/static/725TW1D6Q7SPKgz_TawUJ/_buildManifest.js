544f707d7b602bc6770203d2175f2036887bb726
sxfsq <60212603+sxfsq@users.noreply.github.com>
2025-08-02 17:50:56 +0800

    Add files via upload

---
+++ _next/static/725TW1D6Q7SPKgz_TawUJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/816-572f494ac4679e2b.js","static/chunks/pages/index-8b4dc8986aa0b694.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
